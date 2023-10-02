80e35014b45820a9c050534e1c9854f3fe7aa1bf
Gabriel Silva <gabrielmdhtt@gmail.com>
2023-10-02 18:04:45 -0400

    feat(ACIC-0001): added logging and error handling to quote service

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { MessageService } from './message.service';
 import { Quote } from './Quote';
@@ -22,6 +22,34 @@ export class QuoteService {
 
   /** GET quotes from the server */
   getQuotes(): Observable<Quote[]> {
-    return this.http.get<Quote[]>(this.quoteUrl)
+    return this.http.get<Quote[]>(this.quoteUrl).pipe(
+      tap(_ => this.log('fetched quotes')),
+      catchError(this.handleError<Quote[]>('getQuotes', []))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a QuoteService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`QuoteService: ${message}`);
   }
 }
