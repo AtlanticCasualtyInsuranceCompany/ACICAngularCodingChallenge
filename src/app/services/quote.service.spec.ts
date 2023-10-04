f171be8d190f4ea6cf144fad3a3743fc3748bf6e
Gabriel Silva <gabrielmdhtt@gmail.com>
2023-10-03 20:32:48 -0400

    test(ACIC-0001): added tests for getQuotesByLineOfBusinessId

--- src/app/services/quote.service.spec.ts
+++ src/app/services/quote.service.spec.ts
@@ -47,4 +47,28 @@ describe('QuoteService', () => {
     expect(request.request.method).toBe('GET');
     request.flush(dummyQuotes);
   });
+
+  it('should retrieve quotes for a given lineOfBusinessId', () => {
+    const dummyQuotes: Quote[] = [
+      { id: 101, quoteNumber: 'AC123PC', lineOfBusinessId: 11 },
+      { id: 102, quoteNumber: 'AC124PC', lineOfBusinessId: 12 },
+      { id: 103, quoteNumber: 'AC125PC', lineOfBusinessId: 13 },
+      { id: 104, quoteNumber: 'AC126PC', lineOfBusinessId: 13 },
+      { id: 105, quoteNumber: 'AC127PC', lineOfBusinessId: 15 },
+      { id: 106, quoteNumber: 'AC125PC', lineOfBusinessId: 13 },
+      { id: 107, quoteNumber: 'AC126PC', lineOfBusinessId: 13 },
+      { id: 108, quoteNumber: 'AC127PC', lineOfBusinessId: 15 }
+    ];
+
+    const filteredQuotes = dummyQuotes.filter(quote => quote.lineOfBusinessId === 13);
+
+    service.getQuotesByLineOfBusinessId(13).subscribe(quotes => {
+      expect(quotes.length).toBe(4);
+      expect(quotes).toEqual(filteredQuotes);
+    });
+
+    const request = httpMock.expectOne(service['quoteUrl']);
+    expect(request.request.method).toBe('GET');
+    request.flush(dummyQuotes);
+  });
 });
