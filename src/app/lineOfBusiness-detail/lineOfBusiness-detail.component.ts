641c960bd1358905ceb18bf0f2be7f2017a9c38b
Melecio Silva <mpsilva94@gmail.com>
2023-10-16 14:48:15 -0400

    show number of quotes on details page

--- src/app/lineOfBusiness-detail/lineOfBusiness-detail.component.ts
+++ src/app/lineOfBusiness-detail/lineOfBusiness-detail.component.ts
@@ -1,32 +1,43 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
 
 import { LineOfBusiness } from '../LineOfBusiness';
 import { LineOfBusinessService } from '../lineOfBusiness.service';
+import { Quote } from '../Quote';
+import { QuoteService } from '../Quote.service';
 
 @Component({
   selector: 'app-lineOfBusiness-detail',
   templateUrl: './lineOfBusiness-detail.component.html',
-  styleUrls: [ './lineOfBusiness-detail.component.css' ]
+  styleUrls: ['./lineOfBusiness-detail.component.css']
 })
 export class LineOfBusinessDetailComponent implements OnInit {
   lineOfBusiness: LineOfBusiness | undefined;
+  lineOfBusinessQuotes: Quote[] | undefined;
 
   constructor(
     private route: ActivatedRoute,
     private lineOfBusinessService: LineOfBusinessService,
+    private quoteService: QuoteService,
     private location: Location
   ) {}
 
   ngOnInit(): void {
     this.getLineOfBusiness();
+    this.getRecentQuotesByLineOfBusinessId()
   }
 
   getLineOfBusiness(): void {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.lineOfBusinessService.getLineOfBusiness(id)
       .subscribe(lineOfBusiness => this.lineOfBusiness = lineOfBusiness);
+  }
+
+  getRecentQuotesByLineOfBusinessId(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    this.quoteService.getRecentQuotesByLineOfBusinessId(id)
+      .subscribe(lineOfBusinessQuotes => this.lineOfBusinessQuotes = lineOfBusinessQuotes);
   }
 
   goBack(): void {
